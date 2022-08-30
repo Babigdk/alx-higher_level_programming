97793c1aff93b628cb132f7e5da6ee74ef48e3a5
Babigdk <Babigdk@gmail.com>
2022-08-29 18:37:35 -0700

    m

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    l = len(sys.argv) - 1
+    lo = len(sys.argv) - 1
     print("{} argument{}{}"
-          .format(l, "" if l == 1 else "s", "." if l == 0 else ":"))
-    for i in range(l):
+          .format(lo, "" if lo == 1 else "s", "." if lo == 0 else ":"))
+    for i in range(lo):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
