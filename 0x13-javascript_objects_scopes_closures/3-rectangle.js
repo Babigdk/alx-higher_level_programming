a7a16c2bd06019e9f2f4a2e6ae175e6d84ca2e54
Babigdk <Babigdk@gmail.com>
2022-11-29 11:57:58 -0800

    js core

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,15 +1,20 @@
 #!/usr/bin/node
-// define class Rectangle version 3
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
+    if ((w <= 0) || (h <= 0) || !h || !w) {
+      return this;
     }
+    this.width = w;
+    this.height = h;
   }
+
   print () {
-    for (let j = 0; j < this.height; j++) {
-      console.log(Array(this.width + 1).join('X'));
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
     }
   }
 };
