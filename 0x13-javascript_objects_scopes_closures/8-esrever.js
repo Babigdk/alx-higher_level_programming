5442411c094316d31f0f1e8bf76001d3a540c86a
Babigdk <Babigdk@gmail.com>
2022-11-29 12:13:48 -0800

    js core

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
-// function that returns the reversed version of a list
 exports.esrever = function (list) {
-  let ret = [];
-  for (let i = list.length - 1, j = 0; i >= 0; i--, j++) {
-    ret.push(list[i]);
+  const reversed = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversed.push(list[i]);
   }
-  return ret;
+  return reversed;
 };
