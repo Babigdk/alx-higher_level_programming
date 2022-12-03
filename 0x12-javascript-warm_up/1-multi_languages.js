39f422f360b0c4381caf42efdff5ae0fd7def4fd
Babigdk <Babigdk@gmail.com>
2022-12-03 23:00:16 +0000

    js warm up

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Print 3 lines
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 while (i < 3) {
   console.log(array[i]);
