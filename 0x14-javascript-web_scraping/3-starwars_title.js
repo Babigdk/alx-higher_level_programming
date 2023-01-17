7e14f93b2765bcb134bf0ba97439ca3c9e36b27c
Babigdk <Babigdk@gmail.com>
2023-01-17 18:32:22 +0000

    star war

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
-// Prints the title of a Star Wars movie where the episode number
-// matches the given integer
 
 const request = require('request');
-const argv = process.argv;
-let url = 'https://swapi-api.alx-tools.com/api/films/:id';
 
-request(url + argv[2], function (err, res, body) {
-  if (err) {
-    console.log(err);
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
   }
-  let json = JSON.parse(body);
-  console.log(json.title);
+  console.log(JSON.parse(body).title);
 });
